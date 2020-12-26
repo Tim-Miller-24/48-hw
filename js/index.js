274b94d65132f2d98a1edfac49c355c1814fdf53
Пользователь <erikruka26@gmail.com>
2020-12-26 17:41:59 +0500

    add stop()

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ $('.show').click(function (e) {
 
     $('.hide').removeClass('hidden');
 
-    $('section').slideDown(1000, function () {
+    $('section').stop().slideDown(1000, function () {
         $(this).toggleClass('hidden');
     });
 
@@ -15,29 +15,29 @@ $('.success').click(function (e) {
     e.preventDefault();
 
     if ($('footer').hasClass('hidden')) {
-        $('footer').slideDown(1000, function () {
+        $('footer').stop().slideDown(1000, function () {
             $(this).toggleClass('hidden');
         });
     } else {
-        $('footer').slideUp(1000, function () {
+        $('footer').stop().slideUp(1000, function () {
             $(this).toggleClass('hidden');
         });
     }
 });
 
-$('.hide').click(function (e) {
+$('.hide').stop().click(function (e) {
     e.preventDefault();
 
     $(this).toggleClass('hidden');
 
     $('.show').removeClass('hidden');
 
-    $('section').slideUp(1000, function () {
+    $('section').stop().slideUp(1000, function () {
         $(this).toggleClass('hidden');
     });
 
     if (!$('footer').hasClass('hidden')) {
-        $('footer').slideUp(1000, function () {
+        $('footer').stop().slideUp(1000, function () {
             $(this).toggleClass('hidden');
         });
     }
